59be778f84318fe2ec2dc9e2063ba4eab9377c40
sbijukchhe <skbijukchhe@gmail.com>
2019-08-21 16:23:42 -0400

    Using Page Fragments with Thymeleaf

---
+++ src/main/java/com/example/demo/MyAppsUsingPageFragmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppsUsingPageFragmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppsUsingPageFragmentsApplication.class, args);
+    }
+
+}
